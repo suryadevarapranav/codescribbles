1df2a42124585f32368d11ae694da94851513d91
Pranav Suryadevara <suryadevarapranav28@gmail.com>
2024-07-16 07:12:25 -0500

    Stateless vs Stateful....

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/first_page.dart';
-import 'package:flutter_application_1/pages/home_page.dart';
-import 'package:flutter_application_1/pages/second_page.dart';
-import 'package:flutter_application_1/pages/settings_page.dart';
+import 'package:flutter_application_1/pages/counter_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  List names = ["Mitch", "Sharon", "Vince"];
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      // Routes
-      home: FirstPage(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/homepage': (context) => HomePage(),
-        '/settingspage': (context) => SettingsPage(),
-        '/secondpage': (context) => SecondPage()
-      },
+      home: CounterPage(),
     );
   }
 }
