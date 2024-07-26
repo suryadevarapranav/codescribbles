2606faf0427e78aed510c3e2b261abcefd1db33a
Pranav Suryadevara <suryadevarapranav28@gmail.com>
2024-07-26 17:19:36 -0500

    Finished the Simple Nike UI project

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce/components/cart_item.dart';
+import 'package:ecommerce/models/cart.dart';
+import 'package:ecommerce/models/shoe.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //heading
+            const Text(
+              "My Cart..",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder: (context, index) {
+                  // get individual shoe
+                  Shoe individualShoe = value.getUserCart()[index];
+
+                  // return the cart item
+                  return CartItem(shoe: individualShoe);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
